adfa96818de1a32d0ff01736b2ce493869c8bffd
Radekyspec <w41k3r15347@gmail.com>
2026-01-09 13:47:48 -0500

    fix module export

--- src/server/node-functions/[[server]].js
+++ src/server/node-functions/[[server]].js
@@ -201,9 +201,4 @@ app.get(['/api/leaderboard', '/leaderboard'], async (req, res) => {
     }
 });
 
-module.exports = app;
-if (require.main === module) { //loacl node.js debug
-    app.listen(PORT, '0.0.0.0', () => {
-        console.log(`Bili投票服务器已启动：http://localhost:${PORT}`);
-    });
-}
+export default app;
