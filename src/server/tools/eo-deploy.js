02655b4b8e788917ead52abb99298b15222978c0
Radekyspec <w41k3r15347@gmail.com>
2026-01-09 23:31:10 -0500

    add ioredis version

--- src/server/tools/eo-deploy.js
+++ src/server/tools/eo-deploy.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const cwd = process.cwd();
-const src = path.join(cwd, "server.js");
+const src = path.join(cwd, "server-ioredis.js");
 const destDir = path.join(cwd, "node-functions");
 const dest = path.join(destDir, "[[server]].js");
 
