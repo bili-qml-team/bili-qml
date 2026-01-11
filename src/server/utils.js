ee67e33029087d54b4b88d381be1b0fa5e98249d
ShiroAzusa <azusa@shiroazusa.net>
2026-01-11 15:29:14 +0800

    better code

--- src/server/utils.js
+++ src/server/utils.js
@@ -1,49 +1,43 @@
 class cacheManager{
     mapList=[];
     baseTime=0;
-    constructor(redis, timeSlice, timeInterval){
+    lastUpdate=0;
+    timeSlice=0;
+    constructor(redis, timeBucketIndex){
         this.redis= redis;
-        this.timeInterval= timeInterval;
-        this.timeSlice= timeSlice;
-        let padding=this.timeInterval%this.timeSlice + this.timeInterval
-        this.cacheCount=Math.ceil(Math.log2(padding));
-        this.mapList=[...Array(cacheCount).keys()].map(()=>{return new Map();});
+        this.timeBucketIndex= [0, ...timeBucketIndex];
+        this.cacheCount=this.timeBucketIndex.length;
+        this.mapList=[...Array(this.cacheCount-1).keys()].map(()=>{return new Map();});
+    }
+    async fetchRange(index,slice = this.timeSlice){
+        return await this.redis.zrangebyscore('votes:recent', this.baseTime - this.timeBucketIndex[index] - slice,this.baseTime - this.timeBucketIndex[index]);
     }
     async update(){
-        this.baseTime+= this.timeSlice; // => now
-        let Votes=await Promise.all([...Array(cacheCount).keys()].map(async (index)=>{
-            let targetTime= this.baseTime- this.timeSlice*Math.pow(2,index); // end of a  this.timeSlice
-            return await  this.redis.zrangebyscore('votes:recent',targetTime - this.timeSlice, targetTime);
-        }));
+        this.baseTime=Date.now();
+        this.timeSlice = this.baseTime-this.lastUpdate;
+        this.lastUpdate = this.baseTime;
+        let Votes=await Promise.all([...Array(this.cacheCount).keys()].map(async (index)=>{return this.fetchRange(index)}));
         this.mapList.forEach((item,index)=>{
             for (const member of Votes[index]) {
-                const bvid = member.split(':')[0];  // 从 `${bvid}:${userId}` 提取
+                const bvid = member.split(':')[0];
+                item.set(bvid,(item.get(bvid) || 0) + 1);
+            }
+
+            for (const member of Votes[index+1]) {
+                const bvid = member.split(':')[0];
                 item.set(bvid,(item.get(bvid) || 0) - 1);
-                if(index+1<this.mapList.length){
-                    this.mapList[index+1].set(bvid,(this.mapList[index+1].get(bvid) || 0) + 1);
-                }
             }
         });
-        let headVote=await  this.redis.zrangebyscore('votes:recent', this.baseTime- this.timeSlice,  this.baseTime);
-        for (const member of headVote) {
-                const bvid = member.split(':')[0];
-                this.mapList[0].set(bvid,(this.mapList[0].get(bvid) || 0) + 1);
-            }
-        setTimeout(()=>{this.update()}, this.baseTime+ this.timeSlice-Date.now());
     }
     async init(){
         this.baseTime=Date.now()
-        let Votes=await Promise.all([...Array(cacheCount).keys()].map(async (index)=>{
-            let targetTime= this.baseTime- this.timeSlice*Math.pow(2,index); // end of a  this.timeSlice
-            return await  this.redis.zrangebyscore('votes:recent',targetTime,  this.baseTime);
-        }));
+        this.lastUpdate = this.baseTime;
+        let Votes=await Promise.all([...Array(this.cacheCount-1).keys()].map(async (index)=>{return this.fetchRange(index,this.timeBucketIndex[index+1]-this.timeBucketIndex[index])}));
         this.mapList.forEach((item,index)=>{
             for (const member of Votes[index]) {
                 const bvid = member.split(':')[0];
                 item.set(bvid,(item.get(bvid) || 0) + 1);
             }
         });
-        setTimeout(()=>{this.update()}, this.baseTime+ this.timeSlice-Date.now());
     }
 }
-export default CacheManager;
